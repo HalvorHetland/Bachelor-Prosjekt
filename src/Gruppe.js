e477cf1ee1d663cd0c425db329cb66b78235af38
Halvor Hetland <halvor.hetland@intility.no>
2023-04-27 17:00:15 +0200

    ksks

--- src/Gruppe.js
+++ src/Gruppe.js
@@ -1,8 +1,23 @@
-
-
+import React from 'react';
+import Halvor from './Img/halvor.png';
+import './Gruppe.css';
+import {
+    useNavigate
+  } from "react-router-dom";
+  import ArrowBackIcon from '@mui/icons-material/ArrowBack';
 function Gruppe() {
+        const navigate = useNavigate();
     return(
-<h1>Hello world</h1>
+        <>
+        <div>
+            <h1>Gruppemedlemmer</h1>
+            </div>
+            <div>
+            <ArrowBackIcon onClick={() => navigate(-1)} className='arrowBack' style={{ fontSize: 48 }}/>
+        </div>
+            <div>
+                <img src={Halvor} alt='halvor'/>
+                </div></>
     );
 }
 export default Gruppe;
