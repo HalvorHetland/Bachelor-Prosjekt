bf259079d9ac4a0cdd3bd88a29d708b4a3d29171
Halvor Hetland <halvor.hetland@intility.no>
2023-04-28 10:13:05 +0200

    sada

--- src/Gruppe.js
+++ src/Gruppe.js
@@ -1,19 +1,21 @@
 import React from 'react';
 import Halvor from './Img/halvor.png';
 import './Gruppe.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faArrowLeft } from '@fortawesome/free-solid-svg-icons';
 import {
     useNavigate
   } from "react-router-dom";
   /*import ArrowBackIcon from '@mui/icons-material/ArrowBack';*/
 function Gruppe() {
-        //const navigate = useNavigate();
+        const navigate = useNavigate();
     return(
         <>
         <div>
             <h1>Gruppemedlemmer</h1>
             </div>
             <div>
-        
+            <FontAwesomeIcon icon={faArrowLeft} onClick={() => navigate(-1)} className='arrowBack'/>
         </div>
             <div>
                 <img src={Halvor} alt='halvor'/>
