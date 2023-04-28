2b5489a0ae77001b23f9dbfce4647e606340ef74
Halvor Hetland <halvor.hetland@intility.no>
2023-04-28 13:58:26 +0200

    fsdf

--- src/Gruppe.js
+++ src/Gruppe.js
@@ -1,27 +1,61 @@
 import React from 'react';
 import Halvor from './Img/halvor.png';
+import Ada from './Img/Ada.png';
+import Ersan from './Img/Ersan.png';
+import Hallvard from './Img/Hallvard.png';
+import Kristine from './Img/Kristine.png';
+import Logo from './Img/intility_u_byline_hvit_RGB.svg'
 import './Gruppe.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faArrowLeft } from '@fortawesome/free-solid-svg-icons';
+import Grid from '@mui/material/Grid';
+import Item from '@mui/material/ListItem';
+import Image from '@mui/material/ImageListItem';
 import {
     useNavigate
   } from "react-router-dom";
-  /*import ArrowBackIcon from '@mui/icons-material/ArrowBack';*/
+
 function Gruppe() {
         const navigate = useNavigate();
     return(
         <>
         <div>
-            <h1>Gruppemedlemmer</h1>
-            </div>
-            <div>
-            <FontAwesomeIcon icon={faArrowLeft} onClick={() => navigate(-1)} className='arrowBack'/>
+        <div className='logo'><img src={Logo} alt='logo'/></div>
+        <FontAwesomeIcon icon={faArrowLeft} onClick={() => navigate(-1)} className='arrowBack'/>
         </div>
-            <div>
-                <img src={Halvor} alt='halvor'/>
-                </div></>
+        <div className='container'>
+            <h1>Gruppemedlemmer</h1>
+            <div className='intromedlemmer'><a>Vi var fem medlemmer som jobbet sammen på et bachelorprosjekt hos Intility AS. 
+                Det har vært en svært positiv opplevelse å samarbeide med disse menneskene, da vi alle var svært dedikerte og engasjerte i å utvikle en stand alone webløsning for dokumentering, kalkulering og fakturering av konsulenttjenester. 
+                Vi har lært mye av hverandre og har hatt en god arbeidsprosess gjennom hele prosjektet.
+                Under finner man alle gruppemedlemmer med navn.
+            </a></div>
+        
+            <Grid container spacing={2} columns={{ xs: 1, sm: 2, md: 12 }} className='grid' justifyContent='center'>
+          <Grid item xs={12} sm={6} md={4}>
+            <Image className='halvor'><img src={Halvor} alt='halvor' /></Image>
+            <Item><h3>halvor hetland</h3></Item>
+          </Grid>
+          <Grid item xs={12} sm={6} md={4}>
+            <Image className='halvor'><img src={Ada} alt='halvor' /></Image>
+            <Item><h3>Ada Ruud</h3></Item>
+          </Grid>
+          <Grid item xs={12} sm={6} md={4}>
+            <Image className='halvor'><img src={Ersan} alt='halvor' /></Image>
+            <Item><h3>Ersan Sinani</h3></Item>
+  </Grid>
+  <Grid item xs={12} sm={6} md={4}>
+            <Image className='halvor'><img src={Hallvard} alt='halvor' /></Image>
+            <Item><h3>Hallvard Tjosås Drønen</h3></Item>
+  </Grid>
+  <Grid item xs={12} sm={6} md={4}>
+            <Image className='halvor'><img src={Kristine} alt='halvor' /></Image>
+            <Item><h3>Kristine Opsal</h3></Item>
+  </Grid>
+</Grid> 
+                </div>
+                
+                </>
     );
 }
 export default Gruppe;
-
-//<ArrowBackIcon onClick={() => navigate(-1)} className='arrowBack' style={{ fontSize: 48 }}/>
--- src/Home.js
+++ src/Home.js
@@ -1,6 +1,8 @@
 import { useState } from 'react';
-import Pdf1 from './files/Forprosjektrapport.pdf'
-import { Outlet, Link } from "react-router-dom";
+import Pdf1 from './files/Forprosjektrapport.pdf';
+import Pdf2 from './files/Prosjektskisse gruppe 20.pdf';
+import Pdf3 from './files/STATUS RAPPORT Gruppe  20 (1).pdf';
+import Logo from './Img/intility_u_byline_hvit_RGB.svg';
 import React from 'react';
 import "./Home.css";
 
@@ -8,27 +10,33 @@ function Home() {
   const [cards] = useState([
     {
       title: 'Sluttrapport',
-      text: 'les om vår sluttrapport',
+      text: 'Sluttrapporten gir en fullstendig oversikt over bacheloroppgaven og fungerer som en oppsummering av arbeidet som er gjort. Trykk på knappen for å lese vår sluttrapport',
       button: 'Last ned',
       type: 'sluttrapportpdf'
     },
     {
       title: 'Forprosjektsrapport',
-      text: 'les om vår forprosjektsrapport',
+      text: 'Forprosjektsrapporten ble skrevet i starten av prosjektet og inneholder en beskrivelse av prosjektet og dets mål, samt en analyse av hva som må gjøres for å oppnå disse målene. Trykk på knappen for å lese vår forprosjektsrapport',
       button: 'Last ned',
       type: 'forprosjektpdf'
     },
     {
-      title: 'Prosjekt',
-      text: 'last ned og utforsk vårt prosjekt',
+      title: 'Prosjektskisse',
+      text: 'Prosjektskissen presiserer og definerer bachelorprosjektet på en mest mulig presis måte, og brukes til å bestemme rammebetingelser, krav til utviklingsplattform og andre relevante krav. Trykk på knappen for å lese vår prosjektskisse',
       button: 'Last ned',
-      type : 'zip'
+      type : 'prosjektskisse'
     },
     {
       title: 'Gruppemedlemmer',
       text: 'les mer om våre gruppemedlemmer',
       button: 'Les mer',
       type: 'page'
+    },
+    {
+      title: 'Statusrapport',
+      text: 'Statusrapporten består av informasjon tilknyttet hva vi ønsker å oppnå med bacheloroppgaven. Den består også av diverse annet som hvem vi ønsket å jobbe med og hvilke type arbeid som er optimalt. Trykk på knappen for å lese vår statusrapport',
+      button: 'Last ned',
+      type: 'statusrapport'
     }
   ]);
 
@@ -43,16 +51,27 @@ function Home() {
     else if(type === 'forprosjektpdf'){
       window.open(Pdf1);
     } 
-    else if(type === 'zip'){
-      alert('Ikke ferdig enda');
+    else if(type === 'prosjektskisse'){
+      window.open(Pdf2);
+    }
+    else if(type === 'statusrapport'){
+      window.open(Pdf3);
     }
   };
 
   return (
     <div>
+      <div className='logo'><img src={Logo} alt='logo'/></div>
       <section>
         <div className='container'>
-          <h1>Bacheloroppgave Gruppe 20</h1>
+          <h1>Utvikling av en stand alone webløsning for dokumentering, kalkulering og fakturering av konsulenttjenester
+</h1>
+<div className='innledning'>
+  <a>Intility AS er en bedrift som tilbyr kunden en komplett  plattform tjeneste for multi-cloud IT-miljøer. 
+    Som flere andre bedrifter trenger Intility å holde oversikt på konsulentarbeid. 
+    Vår bacheloroppgave gitt ut på å utvikle et fullt integrert system for å enkelt kunne vedlikeholde og overstyre Intility sine konsulentoppdrag.
+    Utfors diverse dokumenter tilknyttet oppgaven, samt mer informasjon om gruppemedlemmene.</a>
+</div>
           <div className='cards'>
             {
               cards.map((card, i) => (
