3108b85fb5899d083c82751e071340b8842d6c86
kristineopsal <s354527@oslomet.no>
2023-05-10 15:35:52 +0200

    first push

--- src/Home.js
+++ src/Home.js
@@ -2,41 +2,47 @@ import { useState } from 'react';
 import Pdf1 from './files/Forprosjektrapport.pdf';
 import Pdf2 from './files/Prosjektskisse gruppe 20.pdf';
 import Pdf3 from './files/STATUS RAPPORT Gruppe  20 (1).pdf';
-import Logo from './Img/intility_u_byline_hvit_RGB.svg';
+import Logo from './Img/intility_logo_sea-green_RGB.png';
+import Logo2 from './Img/Logomedsignatur_gul_OsloMet-01.png';
 import React from 'react';
 import "./Home.css";
+import Navbar from 'react-bootstrap/Navbar';
+import Halvor from './Img/halvor.png';
+import Ada from './Img/Ada.png';
+import Ersan from './Img/Ersan.png';
+import Hallvard from './Img/Hallvard.png';
+import Kristine from './Img/Kristine.png';
+import './Gruppe.css';
+import Grid from '@mui/material/Grid';
+import Item from '@mui/material/ListItem';
+import Image from '@mui/material/ImageListItem';
+import { Link } from 'react-scroll';
 
 function Home() {
   const [cards] = useState([
     {
-      title: 'Sluttrapport',
-      text: 'Sluttrapporten gir en fullstendig oversikt over bacheloroppgaven og fungerer som en oppsummering av arbeidet som er gjort. Trykk på knappen for å lese vår sluttrapport',
+      title: 'Statusrapport',
+      text: 'Statusrapporten består av informasjon tilknyttet hva vi ønsker å oppnå med bacheloroppgaven. Den består også av diverse annet som hvem vi ønsket å jobbe med og hvilke type arbeid som er optimalt.',
       button: 'Last ned',
-      type: 'sluttrapportpdf'
-    },
-    {
-      title: 'Forprosjektsrapport',
-      text: 'Forprosjektsrapporten ble skrevet i starten av prosjektet og inneholder en beskrivelse av prosjektet og dets mål, samt en analyse av hva som må gjøres for å oppnå disse målene. Trykk på knappen for å lese vår forprosjektsrapport',
-      button: 'Last ned',
-      type: 'forprosjektpdf'
+      type: 'statusrapport'
     },
     {
       title: 'Prosjektskisse',
-      text: 'Prosjektskissen presiserer og definerer bachelorprosjektet på en mest mulig presis måte, og brukes til å bestemme rammebetingelser, krav til utviklingsplattform og andre relevante krav. Trykk på knappen for å lese vår prosjektskisse',
+      text: 'Prosjektskissen presiserer og definerer bachelorprosjektet på en mest mulig presis måte, og brukes til å bestemme rammebetingelser, krav til utviklingsplattform og andre relevante krav.',
       button: 'Last ned',
       type : 'prosjektskisse'
     },
     {
-      title: 'Gruppemedlemmer',
-      text: 'les mer om våre gruppemedlemmer',
-      button: 'Les mer',
-      type: 'page'
+      title: 'Forprosjektsrapport',
+      text: 'Forprosjektsrapporten ble skrevet i starten av prosjektet og inneholder en beskrivelse av prosjektet og dets mål, samt en analyse av hva som må gjøres for å oppnå disse målene.',
+      button: 'Last ned',
+      type: 'forprosjektpdf'
     },
     {
-      title: 'Statusrapport',
-      text: 'Statusrapporten består av informasjon tilknyttet hva vi ønsker å oppnå med bacheloroppgaven. Den består også av diverse annet som hvem vi ønsket å jobbe med og hvilke type arbeid som er optimalt. Trykk på knappen for å lese vår statusrapport',
+      title: 'Sluttrapport',
+      text: 'Sluttrapporten gir en fullstendig oversikt over bacheloroppgaven og fungerer som en oppsummering av arbeidet som er gjort.',
       button: 'Last ned',
-      type: 'statusrapport'
+      type: 'sluttrapportpdf'
     }
   ]);
 
@@ -61,18 +67,38 @@ function Home() {
 
   return (
     <div>
-      <div className='logo'><img src={Logo} alt='logo'/></div>
+      <div className='nav'>
+        <Navbar>
+        <div className='navItems'>
+          <Navbar.Brand className='group'>Gruppe 20</Navbar.Brand>
+          <Link className='omOss' to="sektor2" smooth={true} duration={500}>Dokumenter</Link>
+          <Link className='dokumenter' to="sektor3" smooth={true} duration={500}>Gruppemedlemmer</Link>
+          </div>
+          </Navbar>
+          </div>
+
       <section>
         <div className='container'>
-          <h1>Utvikling av en stand alone webløsning for dokumentering, kalkulering og fakturering av konsulenttjenester
-</h1>
-<div className='innledning'>
-  <a>Intility AS er en bedrift som tilbyr kunden en komplett  plattform tjeneste for multi-cloud IT-miljøer. 
-    Som flere andre bedrifter trenger Intility å holde oversikt på konsulentarbeid. 
-    Vår bacheloroppgave gitt ut på å utvikle et fullt integrert system for å enkelt kunne vedlikeholde og overstyre Intility sine konsulentoppdrag.
-    Utfors diverse dokumenter tilknyttet oppgaven, samt mer informasjon om gruppemedlemmene.</a>
+
+        <div className='sektor1' id="sektor1">
+          
+<div className='intro'>
+<h1>Bachelorprosjekt 2023 - Gruppe 20</h1>
+<h2>Consultant Services</h2>
+  <a>Utvikling av en stand alone webløsning for dokumentering, kalkulering og fakturering av konsulenttjenester</a>
+  
 </div>
+<div className='it-logo'><img src={Logo} alt='logo' style={{ alignSelf: 'center' }}/></div>
+</div>
+
+
+<div className="horizontal-line"></div>
+<div className='sektor2' id="sektor2">
+<div className='card-title' style={{ alignSelf: 'center' }}>
+  <h1>Dokumenter</h1>
+  </div>
           <div className='cards'>
+
             {
               cards.map((card, i) => (
 
@@ -80,17 +106,75 @@ function Home() {
             <h3>
               {card.title}
             </h3>
-            <p>
-              {
-              card.text
-              }
-            </p>
+            <p>{card.text}</p>
             <button className='btn' onClick={() => handleButtonClick(card.type)}>
-              {card.button}
+              Last ned
             </button>
           </div>
           ))
         }
+        </div>
+        </div>
+
+
+<div className="horizontal-line"></div>
+    <div className='sektor3' id="sektor3">
+     <h1>Gruppemedlemmer</h1>
+                <div className='intromedlemmer'><a>Totalt var vi fem medlemmer som jobbet sammen på et bachelorprosjekt hos Intility AS. 
+                    Det har vært en svært positiv opplevelse å samarbeide med disse menneskene, da vi alle var svært dedikerte og engasjerte i å utvikle en stand alone webløsning for dokumentering, kalkulering og fakturering av konsulenttjenester. 
+                    Vi har lært mye av hverandre og har hatt en god arbeidsprosess gjennom hele prosjektet.
+                    Under finner man alle gruppemedlemmer med navn.
+                </a></div>
+            
+                <Grid container spacing={2} columns={{ xs: 1, sm: 2, md: 12 }} className='grid' justifyContent='center'>
+              <Grid item xs={12} sm={6} md={4}>
+                <Image className='group-img' ><img src={Halvor} alt='halvor' /></Image>
+                <Item className='item-container'>
+                <h3>Halvor Hetland</h3> 
+                <p>Anvendt Datateknologi</p>
+                <p>s346096@oslomet.no</p>
+                </Item>
+              </Grid>
+              <Grid item xs={12} sm={6} md={4}>
+                <Image className='group-img'><img src={Ada} alt='ada' /></Image>
+                <Item className='item-container'>
+                <h3>Ada Ruud</h3> 
+                <p>Anvendt Datateknologi</p>
+                <p>s354607@oslomet.no</p>
+                </Item>
+              </Grid>
+              <Grid item xs={12} sm={6} md={4}>
+                <Image className='group-img'><img src={Kristine} alt='kristine' /></Image>
+                <Item className='item-container'>
+                <h3>Kristine Opsal</h3> 
+                <p>Anvendt Datateknologi</p>
+                <p>s354527@oslomet.no</p>
+                </Item>
+      </Grid>
+              <Grid item xs={12} sm={6} md={4}>
+                <Image className='group-img'><img src={Ersan} alt='ersan' /></Image>
+                <Item className='item-container'>
+                <h3>Ersan Sinani</h3> 
+                <p>Anvendt Datateknologi</p>
+                <p>s333944@oslomet.no</p>
+                </Item>
+      </Grid>
+      <Grid item xs={12} sm={6} md={4}>
+                <Image className='group-img'><img src={Hallvard} alt='hallvard' /></Image>
+                <Item className='item-container'>
+                <h3>Hallvard Tjosås Drønen</h3> 
+                <p>Anvendt Datateknologi</p>
+                <p>s354520@oslomet.no</p>
+                </Item>
+      </Grid>
+    </Grid> 
+
+        </div>
+
+        <div className='bunntekst'>
+        <Image className='om-logo'><img src={Logo2} alt='osloMet logo' /></Image>
+        <h5>© OsloMet Bachelor vår 2023 - Gruppe 20</h5>
+
         </div>
         </div>
       </section>
